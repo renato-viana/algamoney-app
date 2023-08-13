f47118af4771436348f8a8b85dc686ddedabf3e1
Renato Borges Viana <renatoviana30@gmail.com>
2023-08-13 12:02:22 -0300

    feat: Permitir Fechar Menu ao Clicar Fora e Não Apenas no Ícone

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,33 +1,47 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { ErrorHandlerService } from '../error-handler.service';
+import { Component, HostListener, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { ErrorHandlerService } from "../error-handler.service";
 
-import { AuthService } from './../../seguranca/auth.service';
+import { AuthService } from "./../../seguranca/auth.service";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.css"],
 })
 export class NavbarComponent implements OnInit {
-
   exibindoMenu: boolean = false;
-  usuarioLogado: string = ''
+  usuarioLogado: string = "";
 
   constructor(
     public auth: AuthService,
     private errorHandler: ErrorHandlerService,
-    private router: Router) { }
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.usuarioLogado = this.auth.jwtPayload?.nome;
   }
-  
+
   temPermissao(permissao: string) {
     return this.auth.temPermissao(permissao);
   }
-  
+
   logout() {
     this.auth.logout();
+  }
+
+  // Método para fechar o menu quando um clique é detectado fora dele
+  fecharMenu(event: MouseEvent) {
+    const targetElement = event.target as HTMLElement;
+    if (!targetElement.closest(".navbar")) {
+      this.exibindoMenu = false;
+    }
+  }
+
+  // Ovinte de eventos para fechar o menu ao clicar fora dele
+  @HostListener("document:click", ["$event"])
+  clickForaMenu(event: MouseEvent) {
+    this.fecharMenu(event);
   }
 }
